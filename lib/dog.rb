b4e3493ef97d448622e2b8a86f955d4d1cf4fa84
Valencia Caldwell <vkcaldwell@gmail.com>
2018-06-18 22:29:27 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,9 +1,19 @@
 class Dog
-  def all
-    @@all = all
+  attr_accessor :name
+  
+  def initalize(all)
+    @@all = []
   end
-  @@all = [ ]
   
   
   
+end
+
+class Song
+ 
+  attr_accessor :name
+ 
+  def initialize(name)
+    @name = name
+  end
 end
